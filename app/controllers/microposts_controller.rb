3ddf06e2e86a2536398e1e2afbe637dbb776c528
Yoshimitsu Soga <yoshimitsu.soga@j-will-corporation.jp>
2016-12-19 09:27:45 +0000

    add post function

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user, only: [:create, :destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -11,6 +11,14 @@ class MicropostsController < ApplicationController
     end
   end
   
+  def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    return redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
+  end
+
   private
   def micropost_params
     params.require(:micropost).permit(:content)
