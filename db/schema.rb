5ecd5547498e4435914b9f33eb02a1ccece37569
Yoshimitsu Soga <yoshimitsu.soga@j-will-corporation.jp>
2016-12-21 14:10:33 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219101337) do
+ActiveRecord::Schema.define(version: 20161219125125) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20161219101337) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.text     "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
