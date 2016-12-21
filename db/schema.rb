f59bcfd0da728d47b614795401eec218ea1ad889
Yoshimitsu Soga <yoshimitsu.soga@j-will-corporation.jp>
2016-12-21 14:22:40 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219125125) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
+ActiveRecord::Schema.define(version: 20161219101337) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -40,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161219125125) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
