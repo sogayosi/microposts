5e65cf7900d67881db264716f58bfaa5f83a9091
Yoshimitsu Soga <yoshimitsu.soga@j-will-corporation.jp>
2016-12-25 11:45:51 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219125125) do
 ActiveRecord::Schema.define(version: 20161219101337) do
 
   create_table "microposts", force: :cascade do |t|
