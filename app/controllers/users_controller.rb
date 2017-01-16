08fa065ba5d64091b546df56743929570394a7fe
Yoshimitsu Soga <yoshimitsu.soga@j-will-corporation.jp>
2017-01-16 04:53:31 +0000

    add list for follower/followed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
    @microposts = @user.microposts.order(created_at: :desc)
+   @following_users = @user.following_users
+   @follower_users = @user.follower_users
   end
   
   def new
@@ -36,6 +38,7 @@ class UsersController < ApplicationController
     end
   end
   
+
   
   private
 
